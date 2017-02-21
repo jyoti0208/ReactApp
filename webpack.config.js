22fb18db18695fbe5fcfad6b7d31f6d026d6e5d7
jyoti0208 <sjyotisingh220@gmail.com>
2017-02-21 11:53:42 +0530

    Simple react component added

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+import path from 'path';
+import webpack from 'webpack';
+
+export default {
+  devtool: 'eval-source-map',
+  entry: path.join(__dirname, 'client/index.js'),
+  output: {
+    path: '/',
+    publicPath: '/',
+    filename: './bundle.js'
+  },
+  module: {
+     loaders: [
+       {
+         test: /\.js$/,
+         include: path.join(__dirname, 'client'),
+         loaders: [ 'babel-loader' ]
+       }
+     ]
+   },
+   resolve: {
+     extensions: [ '.js' ]
+   }
+}
