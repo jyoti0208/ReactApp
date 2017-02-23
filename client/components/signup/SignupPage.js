bc22a610edb696d57ac1ea720229540e549759a6
jyoti0208 <sjyotisingh220@gmail.com>
2017-02-24 00:54:39 +0530

    router added with navigation

---
+++ client/components/signup/SignupPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class SignupPage extends React.Component {
+  render() {
+    return (
+      <div>
+      <h1>Sign up Page</h1>
+      </div>
+    );
+  }
+}
+export default SignupPage;
